50e9ccdfcb08bfcd08aec3f876eb27c5214fa8ed
Shariful Islam <engr.shariful.cse@gmail.com>
2022-11-13 03:12:31 +0100

    modified the banner

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -5,11 +5,16 @@ import PrimaryButton from "../../../components/PrimaryButton";
 
 const Banner = () => {
   return (
-    <div className="hero" style={{ backgroundImage: `url(${bg})` }}>
-      <div className="hero-content flex-col lg:flex-row-reverse justify-between">
+    <div className="hero 	" style={{ backgroundImage: `url(${bg})` }}>
+      <div
+        className="hero-content flex-col lg:flex-row-reverse justify-between"
+        style={{ height: "600px" }}
+      >
         <img src={chair} className="max-w-sm rounded-lg shadow-2xl" alt="" />
         <div className="w-1/2 ">
-          <h1 className="text-5xl font-bold">Box Office News!</h1>
+          <h1 className="text-5xl font-bold">
+            Your New Smile Starts <br /> Here
+          </h1>
           <p className="py-6">
             Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
             excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
